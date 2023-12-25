492be544c67f8c2feda3403cefbbb135e56d3c62
Heewon-K <104782645+Heewon-K@users.noreply.github.com>
2023-12-26 02:28:00 +0900

    [BOJ] 괄호 / STK / 35

---
+++ hwkim/BOJ_9012.py
@@ -0,0 +1,26 @@
+# [BOJ] 괄호 / STR / 35
+
+import sys 
+from collections import deque
+n = int(input())  # 첫 줄에서 명령어 갯수를 입력 받음
+a = [sys.stdin.readline().rstrip() for i in range(n)] # n줄 입력 
+
+def search(x):
+    stack = []
+    for item in x:
+        if item == '(':
+            stack.append(item)
+        elif item == ')':
+            if not stack or stack[-1] != '(':
+                print('NO')
+                return
+            else:
+                stack.pop()
+    if not stack:
+        print('YES')
+    else:
+        print('NO')
+
+   
+for x in a:
+    search(x)
